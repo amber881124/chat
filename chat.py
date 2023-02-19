22ee7114d08b44a2901acdfbb9847fcf9e9dacad
momo <zyongzhu24@gmail.com>
2023-02-19 22:40:57 +0800

    refactor

--- chat.py
+++ chat.py
@@ -1,15 +1,39 @@
-chat = []
-with open('input.txt', 'r', encoding = 'utf-8') as f:
-    for line in f:
-        if 'Allen' in line:
-            name = 'Allen'
-            continue
-        elif 'Tom' in line:
-            name = 'Tom'
-            continue
-        else:
-            chat.append([name, line])
+# 讀取檔案
+def readfile(rfilename):
+    chat = []
+    with open(rfilename, 'r', encoding = 'utf-8-sig') as f:
+        for line in f:
+            chat.append(line.strip())
+        print(chat)
+        return chat
 
-with open('output.txt', 'w', encoding = 'utf-8') as f:
-    for name, line in chat:
-        f.write(f'{name}: {line}')
+# 內容格式轉換
+def convert(chat):
+    new = []
+    person = None # 先不給值(但還是要先宣告這個變數，以免出錯)
+    for line in chat:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person: # 如果person有值
+            new.append([person, line])
+    return new
+
+# 寫入檔案
+def write_file(wfilename, chat):
+    with open(wfilename, 'w', encoding = 'utf-8') as f:
+        for person, line in chat:
+            f.write(f'{person}: {line}\n')
+
+def main():
+    rfilename = 'input.txt'
+    wfilename = 'output.txt'
+    chat = readfile(rfilename)
+    chat = convert(chat)
+    write_file(wfilename, chat)
+
+if __name__ == '__main__':
+    main()
